979a327edb290ad080260d20669c7842b14340a5
Tomas Menegol <tomas.menegol@gmail.com>
2024-05-16 11:26:49 -0300

    H por la mitad

--- Practica1/src/resolucion/Fraccion.java
+++ Practica1/src/resolucion/Fraccion.java
@@ -49,11 +49,44 @@ public class Fraccion {
     	int den = q1.denominador * q2.denominador;
     	
     	Fraccion resultado = new Fraccion(num, den);
-    	int mcd = Acts32_35.mcd(resultado.numerador, resultado.denominador);
-    	
-    	resultado.numerador = resultado.numerador /mcd;
-    	resultado.denominador = resultado.denominador/mcd;
+    	resultado.reducir();
+//    	int mcd = Acts32_35.mcd(resultado.numerador, resultado.denominador);
+//    	
+//    	resultado.numerador = resultado.numerador /mcd;
+//    	resultado.denominador = resultado.denominador/mcd;
     	
     	return resultado;
+    }
+    
+    //Menor Comun Multiplo
+    public int mcm(int a, int b) {
+    	int[] multiplosA = new int[b];
+    	int[] multiplosB = new int[a];
+    	
+    	for(int i=1; i<= a*b;i++) {
+    		multiplosA[i] = a*i;
+    		multiplosB[i] = b*i;
+    	}
+    	
+    	for(int i = 1; i<multiplosA.length;i++){
+    		for(int j = 1; j<multiplosB.length;j++){
+        		if(multiplosA[i] == multiplosB[j]) {
+        			return multiplosA[i];
+        		}
+        	}
+    	}
+    	
+    	return a*b;
+    }
+    
+    //H
+    static public Fraccion suma(Fraccion q1, Fraccion q2) {
+    	if(q1.denominador == q2.denominador) {
+    		Fraccion resultado = new Fraccion (q1.numerador + q2.numerador, q1.denominador);
+    		resultado.reducir();
+    		return resultado;
+    	}else{
+    		int num = ;
+    	}
     }
 }
--- Practica1/src/resolucion/Main.java
+++ Practica1/src/resolucion/Main.java
@@ -2,9 +2,22 @@ package resolucion;
 
 public class Main {
 	public static void main(String[] args){
+		//A
 		Fraccion f = new Fraccion(9,10);
+		//B
+		f.imprimir();
+		//C
+		f.invertirSigno();f.imprimir();
+		//D
+		f.invertir();f.imprimir();
+		//E
+		System.out.println(f.aDouble());
+		//F
+		f.reducir();f.imprimir();
+		//G
 		Fraccion g = new Fraccion(20,30);
 		System.out.print(Fraccion.producto(f, g).numerador + "/" + Fraccion.producto(f, g).denominador);
+		//H
 		
 		
 	}
