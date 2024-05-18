75bfb193e619491cc97dc6d254607b0b4d627141
Tomas Menegol <tomas.menegol@gmail.com>
2024-05-18 02:08:35 -0300

    2 hecho

--- Practica1/src/resolucion/Main.java
+++ Practica1/src/resolucion/Main.java
@@ -2,27 +2,36 @@ package resolucion;
 
 public class Main {
 	public static void main(String[] args){
-		//1)A
-		Fraccion f = new Fraccion(9,10);
-		
-		f.editarNum(32);
-		//1)B
-		f.imprimir();
-		//1)C
-		f.invertirSigno();f.imprimir();
-		//1)D
-		f.invertir();f.imprimir();
-		//1)E
-		System.out.println(f.aDouble());
-		//1)F
-		f.reducir();f.imprimir();
-		//1)G
-		Fraccion g = new Fraccion(20,30);
-		Fraccion.producto(f, g).imprimir();
-		//1)H
-		Fraccion.suma(f, g).imprimir();
+//		//1)A
+//		Fraccion f = new Fraccion(9,10);
+//		
+//		// Probar setter
+		//f.editarNum(32);
+//		//1)B
+//		f.imprimir();
+//		//1)C
+//		f.invertirSigno();f.imprimir();
+//		//1)D
+//		f.invertir();f.imprimir();
+//		//1)E
+//		System.out.println(f.aDouble());
+//		//1)F
+//		f.reducir();f.imprimir();
+//		//1)G
+//		Fraccion g = new Fraccion(20,30);
+//		Fraccion.producto(f, g).imprimir();
+//		//1)H
+//		Fraccion.suma(f, g).imprimir();
 		
 		//2A
-		
+		Punto p = new Punto();
+		//2B
+		Punto g = new Punto(150.3,230.5);
+		//2C
+		p.imprimir();g.imprimir();
+		//2D
+		p.desplazar(16, 31.5); p.imprimir();
+		//2E
+		Punto.distancia(p, g).imprimir();;
 	}
 }
--- Practica1/src/resolucion/Punto.java
+++ Practica1/src/resolucion/Punto.java
@@ -10,13 +10,46 @@ public class Punto {
 		this.y = 0;
 	}
 	
-	
 	//B
 	public Punto(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	//C
+//	//Setter Si hago las variables privadas como para usar esto, se rompe todo 
+//	public void setx(double a) {
+//		this.x = a;
+//	}
+//	
+//	public void sety(double a) {
+//		this.y = a;
+//	}
+//	
+//	//Getters
+//	public double getx() {
+//		return this.x;
+//	}
+//	
+//	public double gety() {
+//		return this.y;
+//	}
 	
+	//C
+	public void imprimir() {
+		System.out.println("(" + x + ";" + y + ")");
+	}
+	
+	//D
+	public void desplazar(double desp_x, double desp_y) {
+		this.x += desp_x;
+		this.y += desp_y;
+	}
+	
+	//E
+	static public Punto distancia(Punto p1, Punto p2) {
+		double x = p1.x - p2.x;
+		double y = p1.y - p2.y;
+		Punto w = new Punto(x,y);
+		return w;
+	}
 }
